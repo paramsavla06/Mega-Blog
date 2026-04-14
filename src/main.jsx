9274dfeeca1914679f23f78e77ab2128f75593f9
paramsavla06 <paramsavla06@gmail.com>
2026-04-14 12:47:01 +0530

    fix: router provider

--- src/main.jsx
+++ src/main.jsx
@@ -76,9 +76,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 )
